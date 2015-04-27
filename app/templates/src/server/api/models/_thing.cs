c25ef271843af9e7993aca716a48c12813e4f286
Dolan Miu <dolan_miu@hotmail.com>
2015-04-27 14:33:55 +0100

    added a thing model

---
+++ app/templates/src/server/api/models/_thing.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace <%= title %>.Server.API.Models
+{
+    public class Thing
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Logo { get; set; }
+    }
+}
